168217359f427f38ac713d1ea1529be9635c0da1
bishalrajbanshi <bishalrajbanshi.in@gmail.com>
2026-03-01 17:05:56 +0545

    Refactor Role module and controller to enhance functionality and improve code structure

--- src/main.ts
+++ src/main.ts
@@ -7,5 +7,7 @@ async function bootstrap() {
   await app.listen(process.env.PORT ?? 3000);
 
   app.useGlobalFilters(new AllExceptionFilter());
+  app.setGlobalPrefix('api');
+ console.log(`Listening on http://0.0.0.0:${process.env.PORT ?? 3000}`);
 }
 bootstrap();
