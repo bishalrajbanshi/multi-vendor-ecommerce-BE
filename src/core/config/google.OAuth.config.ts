492cefb2e5f0fd83fe006a640477f1e3c8d25c5a
bishalrajbanshi <bishalrajbanshi.in@gmail.com>
2026-03-06 19:01:10 +0545

    chenaged

--- src/core/config/google.OAuth.config.ts
+++ src/core/config/google.OAuth.config.ts
@@ -1,7 +1,7 @@
 export default (): any => ({
   GOOGLE_OAUTH: {
-    CLIENT_ID: process.env.CLIENT_ID as string,
-    CLIENT_SECRET: process.env.CLIENT_SECRET as string,
-    CALL_BACK_URL: process.env.CALL_BACK_URL as string,
+    CLIENT_ID: process.env.GOOGLE_CLIENT_ID as string,
+    CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET as string,
+    CALL_BACK_URL: process.env.GOOGLE_CALLBACK_URL as string,
   },
 });
